625e208a477642de0875ffa40b3199f6adf8f17d
Nauroz Ahmad <ahmadnauroz@gmail.com>
2024-11-07 11:49:58 +0500

    feat: make chatbot responsive.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,14 +7,19 @@ import { Footer } from '@/components/Footer';
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   useScrollToTop();
 
+  const isMobile = window.innerWidth < 768;
+
   return (
     <>
       <Navbar />
       {children}
 
-      <div className="fixed bottom-0 right-0" style={{ width: '27rem', height: '37rem', zIndex: 1000 }}>
+      <div
+        className="fixed bottom-0 right-0"
+        style={{ width: isMobile ? '100%' : '27rem', height: isMobile ? '100%' : '37rem', zIndex: 1000 }}
+      >
         <iframe
-          src="https://pandy-bot.vercel.app"
+          src={`https://pandy-bot.vercel.app?deviceType=${isMobile ? 'mobile' : 'desktop'}`}
           width="100%"
           height="100%"
           style={{ border: 'none' }}
